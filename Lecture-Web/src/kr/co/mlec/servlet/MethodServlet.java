87fa857400176d38fc871b299fe496e6f978a091
DESKTOP-9EPT6E9\user <lop903641@gmail.com>
2022-01-10 16:06:23 +0900

    0110

---
+++ Lecture-Web/src/kr/co/mlec/servlet/MethodServlet.java
@@ -0,0 +1,50 @@
+package kr.co.mlec.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MethodServlet extends HttpServlet {
+    
+	// http://localhost:9999/Lecture-Web/method?id=aaa
+	@Override   
+	                                         //요청 변수                                응답변수
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+	
+	  String method =	request.getMethod();
+      String uri = request.getRequestURI(); //9999~이후것들만 가져오는것
+      String id =request.getParameter("id");
+      
+      
+      
+      response.setContentType("text/html; charset=utf-8");
+      PrintWriter out = response.getWriter();
+      
+      out.println("<HTML>"); //클라이언트에게 전송하겠다
+      out.println("     <HEAD>"); 
+      out.println("<TITLE>메소드 호출결과</TITLE"); 
+      
+      out.println("   </HEAD>"); 
+      out.println("   <BODY>"); 
+      out.println("===============================<br>");
+      out.println("&nbsp;&nbsp;&nbsp; 출 력 결 과<br>");
+      out.println("요청 메소드 :"+ method+ "<br>");
+      out.println("요청 URI :"+ uri+ "<br>");
+      out.println("파라미터(id)" + id+ "<br>");
+      out.println("===============================<br>");
+      out.println("   </BODY>"); 
+      out.println("</HTML>"); 
+      
+      /*
+      System.out.println("요청 메소드 : " +method);
+      System.out.println("요청 URI : " +uri);
+      System.out.println("id : " +id);
+	  */
+	  
+	}
+}

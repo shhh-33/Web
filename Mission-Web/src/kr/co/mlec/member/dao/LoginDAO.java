6d0e738702eaaad516716635665462a648e40913
DESKTOP-9EPT6E9\user <lop903641@gmail.com>
2022-01-14 20:19:33 +0900

    0114

--- Mission-Web/src/kr/co/mlec/member/dao/LoginDAO.java
+++ Mission-Web/src/kr/co/mlec/member/dao/LoginDAO.java
@@ -4,49 +4,75 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import kr.co.mlec.board.vo.BoardVO;
 import kr.co.mlec.member.vo.LoginVO;
 import kr.co.mlec.util.ConnectionFactory;
 
 public class LoginDAO {
 	/*
 	 * 로그인 서비스
+	 * 
 	 * @param loginVO (사용자가 입력한 id,password 저장)
-	 * @return userVO ( id, password로 조회된 회원정보)
-	 * 조회된 정보가 없다면 null 반환 
+	 * 
+	 * @return userVO ( id, password로 조회된 회원정보) 조회된 정보가 없다면 null 반환
 	 * 
 	 * 
 	 * 
 	 */
 
 	public LoginVO login(LoginVO loginVO) {
-		
-		LoginVO userVO =null;
-		
+
+		LoginVO userVO = null;
+
 		StringBuilder sql = new StringBuilder();
 		sql.append("select id, name, password, type ");
 		sql.append("  from tbl_member2 ");
 		sql.append("where id =? and password = ? ");
-		
+
 		try (Connection conn = new ConnectionFactory().getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql.toString());
 
 		) // 1.7버전 :()안에 autocloaseable 변수 ->finally안하려고
 		{
-          pstmt.setString(1, loginVO.getId());
-          pstmt.setString(2, loginVO.getPassword());
-          
-          ResultSet rs =pstmt.executeQuery();
-          if(rs.next()) {
-        	  userVO = new LoginVO();
-        	  userVO.setId(rs.getString("id"));
-        	  userVO.setPassword(rs.getString("password"));
-        	  userVO.setType(rs.getString("type"));
-          }
-          
+			pstmt.setString(1, loginVO.getId());
+			pstmt.setString(2, loginVO.getPassword());
+
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				userVO = new LoginVO();
+				userVO.setId(rs.getString("id"));
+				userVO.setPassword(rs.getString("password"));
+				userVO.setType(rs.getString("type"));
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-         return userVO;
+		return userVO;
+	}
+
+	
+	public void updateCnt(BoardVO boardVO) {
+
+		BoardVO board = null;
+
+		StringBuilder sql = new StringBuilder();
+		sql.append("select  ");
+		
+		
+		
+		
+		try (Connection conn = new ConnectionFactory().getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(board);
+
+		) 
+		{ pstmt.setInt(1, boardVO.getViewCnt());
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
